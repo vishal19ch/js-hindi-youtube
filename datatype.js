7f64eb26a03075ccce4e04ee177e595c2e87ac97
vishal19ch <88112970+vishal19ch@users.noreply.github.com>
2024-08-01 04:33:43 +0000

    conversion of data

---
+++ datatype.js
@@ -0,0 +1,28 @@
+// let a=null;
+
+// console.log( typeof a);//type is coming number
+
+// let value=Number(a);
+// console.log( typeof value); //value is comming String
+
+
+
+// let c="1add";
+
+// console.log( typeof a);//type is object
+
+// let value1=Number(c);
+// console.log( typeof value1); //value is comming String
+
+// console.log(c);
+// console.log(value1);
+
+
+//"abc123" value given Nan
+
+
+let number="1"; //if we pass 1 or string return true  if we pass null return false
+
+let result=Boolean(number);
+
+console.log(result);
